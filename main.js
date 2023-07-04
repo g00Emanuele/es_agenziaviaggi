e9723cde53dab97576a4b9b22016c78860358cb7
g00Emanuele <g00emanuele@gmail.com>
2023-07-04 17:29:01 +0200

    Commit js completo, grafica incompleta

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-const myModal = new bootstrap.Modal(document.getElementById('myModal'), options)
+const button = document.getElementById('removeCard')
+const card = document.querySelectorAll('.card')
+
+button.addEventListener('click', () => {
+    for (let i = 0; i < card.length; i++) {
+        let a = card[i]
+        a.classList.add('d-none')
+    }
+})
+
+
+
+const  viaggi = document.getElementsByClassName('custom-trip')
+const numeroViaggi = document.getElementById('viaggi')
+
+addEventListener("load", () => {
+    let count = 0
+    for (const i of viaggi) {
+        count ++
+    }
+    numeroViaggi.innerText = count
+});
+
